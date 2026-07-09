e056b80359d24e46910b1846c9ba3a81094bbc2a
Farkas Gergely <farkas.szte@gmail.com>
2026-07-09 10:27:50 +0200

    chore: bump application version from 3.5 to 3.6 in utils.py
    Update the VERSION constant in utils.py to 3.6 to reflect the
    latest release and maintain accurate version reporting for
    the video player application.

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import sys
 import json
 import subprocess
 from PyQt6.QtCore import Qt
-VERSION = "3.5"
+VERSION = "3.6"
 
 def get_base_path():
     """ Get the directory where the application is located (next to .exe if bundled) """
